9fa944c49501cbbb422aa72be6902ba429027fa2
crocoya <yassine.tababi@outlook.com>
2023-02-15 10:30:38 +0100

    adding content to the recipes components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes: [] = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://data.thefeedfeed.com/static/2019/06/14/15605265545d03beda5eb6f.JPG'
+    ),
+  ];
 
   constructor() {}
 
